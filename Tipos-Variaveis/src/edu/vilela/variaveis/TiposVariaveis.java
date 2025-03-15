e795fbac166098cbd3c30c817678e1e9671df6e3
Day-vilela <dayamano64@gmail.com>
2025-03-15 20:37:33 -0300

    terminal, argumentos, docs

--- Tipos-Variaveis/src/edu/vilela/variaveis/TiposVariaveis.java
+++ Tipos-Variaveis/src/edu/vilela/variaveis/TiposVariaveis.java
@@ -1,32 +1,32 @@
 package edu.vilela.variaveis;
-public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá, Vilela! Seja muito Bem-Vinda!");
+        /**
+          OS 8 TIPOS PRIMITIVOS EM JAVA
+          - int, byte, short, float, long, double e char : são considerados objetos. Portanto representam valores brutos. São armzenados diretamente na pilha de memória. 
+         
 
-        /*
-         * OS 8 TIPOS PRIMITIVOS EM JAVA
-         *  - int, byte, short, float, long, double e char : são considerados objetos. Portanto representam valores brutos. São armzenados diretamente na pilha de memória. 
-         */
+         double salarioMinimo = 2500;
 
-         /*double salarioMinimo = 2500;
-
-         // PECULIARIDADES
+         PECULIARIDADES
 
          byte idade = 123;
          short ano = 2021;
          int cep = 26050195;
          long cpf = 19019552734L;
          float pi = 3.14F;
-         double salario = 1275.33; */
+         double salario = 1275.33; 
 
-        // CONSTANTES E VARIAVEIS 
+         CONSTANTES E VARIAVEIS 
 
-        /*
-         *  - CONSTANTES: não podem ser modificadas. 
-         * sua convenção se da pela paçavra "FINAL" ou  suas variáveis escritas em "CAIXA ALTA"
-         * 
-         * - VARIÁVEIS COMUNS: podem ser modificadas.  
+        
+          - CONSTANTES: não podem ser modificadas. 
+          sua convenção se da pela paçavra "FINAL" ou  suas variáveis escritas em "CAIXA ALTA"
+          
+         - VARIÁVEIS COMUNS: podem ser modificadas.  
          */
+
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá, Vilela! Seja muito Bem-Vinda!");
  
         int numero = 5;
         numero = 10; // foi possivel modificar
